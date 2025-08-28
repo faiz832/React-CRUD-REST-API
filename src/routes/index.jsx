5d408092e0bdaf492d3238d61ff440937d965597
faiz832 <faqodefazz@gmail.com>
2025-08-28 12:29:49 +0700

    feat: add home-page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Product from "../pages/Product";
+import Home from "../pages/Home";
 
 export default function index() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
       </Routes>
     </Router>
